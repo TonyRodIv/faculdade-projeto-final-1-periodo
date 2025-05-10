3f91b07b306ddd437c0e58579be213d6799b686d
TonyRodIv <antony.rodriguesiv@gmail.com>
2025-05-10 17:51:48 -0300

    Atualiza a interface do aplicativo Open Tickets, ajustando o modo de aparência e a geometria da janela, além de reorganizar os botões e adicionar títulos.

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import customtkinter as ctk
 
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("blue")
+ctk.set_appearance_mode("system")
+ctk.set_default_color_theme("blue")  
 
 app = ctk.CTk()
-app.geometry("400x250")
+app.geometry("500x350")
+app.title("Open Tickets App")
 
-btn = ctk.CTkButton(app, text="Botão 1", command=lambda: print("Você clicou no botão 1!"))
-btn.pack(padx=20, pady=20)
-btn = ctk.CTkButton(app, text="Botão 2", command=lambda: print("Você clicou no botão 2!"))
-btn.pack(padx=20, pady=20)
+
+title = ctk.CTkLabel(app, text="Bem vindo ao Open Tickets", font=("Arial", 18, "normal"), wraplength=400)
+title.pack(pady=5)
+title = ctk.CTkLabel(app, text="Qual tipo de usuário vai acessar a aplicação?", font=("Arial", 22, "bold"), wraplength=400)
+title.pack(pady=10)
+
+btn = ctk.CTkButton(app, text="Administrador", fg_color="purple", command=lambda: print("Você clicou no botão 1!"))
+btn.pack(padx=10, pady=10)
+btn = ctk.CTkButton(app, text="Vendedor", command=lambda: print("Você clicou no botão 2!"))
+btn.pack(padx=10, pady=10)
 
 app.mainloop()
